8ec09ac6908d67b34c0ebf87e0996c8ff1d93813
Heronilde <nildocossamz@gmail.com>
2025-12-19 10:39:41 +0200

    trocar shared

--- lib/shared.dart
+++ lib/shared.dart
@@ -1,8 +1,11 @@
 /// Support for doing something awesome.
 ///
 /// More dartdocs go here.
-library;
+///library;
 
-export 'src/shared_base.dart';
+//export 'src/shared_base.dart';
+
+export 'src/modules/user/data/model/user_model.dart';
+export 'src/modules/user/ui/provider/user_provider.dart';
 
 // TODO: Export any libraries intended for clients of this package.
